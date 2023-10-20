9b95de3230f097f079f2637b85af63354cfd5025
neetuchoudhary021993 <132840941+neetuchoudhary021993@users.noreply.github.com>
2023-10-20 13:03:26 +0530

    Update store.js

--- src/store.js
+++ src/store.js
@@ -1,1 +1,14 @@
 //code here
+
+// store.js
+
+import { configureStore } from '@reduxjs/toolkit';
+import zodiac from './reducers';
+
+const store = configureStore({
+  reducer: {
+    zodiac: ZodiacReducer,
+  },
+});
+
+export default store;
